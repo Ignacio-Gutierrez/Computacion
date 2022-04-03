600edaa4cd8236d8844b0a781f7c06ada5088d59
Ignacio <90201295+IJGutierrez@users.noreply.github.com>
2022-04-02 21:48:26 -0300

    Delete Clase_1 directory

--- Clase_1/main.py
+++
@@ -1,4 +0,0 @@
-from suma import suma
-if __name__ == "__main__":
-    resultado = suma(3, 3)
-    print(resultado)
--- Clase_1/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from suma import suma
-class TestCalculator(unittest.TestCase):
-    def testSuma(self):
-        resultado = suma(2, 3)
-        self.assertEqual(5,resultado)
-
-if __name__ == "__main__":
-    unittest.main()
