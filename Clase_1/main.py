e3dfb0b0e7d7b6f9f2b1cb1b7fdd6e25ab898d5f
IJGutierrez <ij.gutierrez@alumno.um.edu.ar>
2022-04-02 22:58:42 -0300

    commit Clase_1

---
+++ Clase_1/main.py
@@ -0,0 +1,5 @@
+from suma import suma
+
+if __name__ == "__main__":
+    resultado = suma(3, 3)
+    print(resultado)
---
+++ Clase_1/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from suma import suma
+class TestCalculator(unittest.TestCase):
+    def testSuma(self):
+        resultado = suma(2, 3)
+        self.assertEqual(5,resultado)
+
+if __name__ == "__main__":
+    unittest.main()
