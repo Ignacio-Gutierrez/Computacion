d28ca0726e7be090848079f0007b53153dc043a4
IJGutierrez <ij.gutierrez@alumno.um.edu.ar>
2022-04-06 21:17:23 -0300

    intento final "Strong Password"

---
+++ Clase_4/StrongPassword.py
@@ -0,0 +1,18 @@
+
+def minimumNumber(n, password):
+
+    special_characters = "!@#$%^&*()-+"
+    res = 0
+
+    if any(x.isupper() for x in password) == False:
+        res += 1
+    if any(x.islower() for x in password) == False:
+        res += 1
+    if any(x.isdigit() for x in password) == False:
+        res += 1
+    if any(x in special_characters for x in password) == False:
+        res += 1
+
+    return max(res,6 - n)
+
+    
