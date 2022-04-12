e64043a0e4c294519eb3baa113b3c83457f1389e
IJGutierrez <ij.gutierrez@alumno.um.edu.ar>
2022-04-12 12:03:41 -0300

    referencia a objeto

---
+++ Clase_5/persona.py
@@ -0,0 +1,5 @@
+class Persona:
+    def __init__(self, dni = 1, apellido = 'Fernandez', nombre = 'Alberto'):
+        self.dni = dni
+        self.apellido = apellido
+        self.nombre = nombre
---
+++ Clase_5/test2_persona.py
@@ -0,0 +1,14 @@
+import unittest
+
+from persona import Persona
+
+class TestPersona(unittest.TestCase):
+    def test_persona(self):
+        persona = Persona()
+        print(persona)
+        persona = Persona()
+        print(persona)
+        self.assertEqual(persona.__dict__, {'dni': 1,'apellido': 'Fernandez', 'nombre': 'Alberto'})
+
+if __name__ == '__main__':
+    unittest.main()
