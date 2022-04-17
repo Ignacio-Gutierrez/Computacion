70be244da508ee2172f177cebf4057332be22ea4
IJGutierrez <ij.gutierrez@alumno.um.edu.ar>
2022-04-17 20:03:30 -0300

    hackranck

---
+++ Clase_hackerrank/Compare the Triplets.py
@@ -0,0 +1,12 @@
+def compareTriplets(a, b):
+
+    alice = 0
+    bob = 0
+
+    for i in range(3):
+        if a[i]>b[i]:
+            a += 1
+        elif a[i]<b[i]:
+            b += 1
+            
+    return [alice, bob]
---
+++ Clase_hackerrank/Plus Minus.py
@@ -0,0 +1,17 @@
+def plusMinus(arr):
+    
+    positive = 0
+    negative = 0
+    zero = 0
+
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            positive += 1
+        elif arr[i] < 0:
+            negative += 1
+        else:
+            zero += 1
+    
+    print(positive/len(arr))
+    print(negative/len(arr))
+    print(zero/len(arr))
